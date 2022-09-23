8a76deea0bebbba41796e7bbd84d0dc6314c6b72
Mugisha-Beline <belinemugisha21@gmail.com>
2022-09-23 09:22:32 +0200

    date of birth checks

--- test_regex.py
+++ test_regex.py
@@ -61,13 +61,13 @@ def password_checker():
 
 def DOB_checker():
     """ Check if email is valid """
-    match = re.compile(r"")  # Your Regex goes between the double quotes
-    email = input("Enter the Email: ")
+    match = re.compile(r"/^(0?[1-9]|[12][0-9]|3[01])[/._-](0?[1-9]|1[012])[/._-]\d{4}$/")  # Your Regex goes between the double quotes
+    DOB = input("Enter the date of birth: ")
 
-    if re.fullmatch(match, email):
-        print(f"'{email}'looks like a valid Email")
+    if re.fullmatch(match, DOB):
+        print(f"'{DOB}'looks like a valid date of birth")
     else:
-        print("Invalid Email")
+        print("Invalid date of birth")
 
 
 def phone_number_checker():
