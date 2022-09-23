f6e757e1038a08914e84d750b188539ca1f53bf1
Mugisha-Beline <belinemugisha21@gmail.com>
2022-09-23 09:49:43 +0200

    added separator

--- test_regex.py
+++ test_regex.py
@@ -61,7 +61,7 @@ def password_checker():
 
 def DOB_checker():
     """ Check if email is valid """
-    match = re.compile(r"/^(0?[1-9]|[12][0-9]|3[01])[/._-](0?[1-9]|1[012])[/._-]\d{4}$/")  # Your Regex goes between the double quotes
+    match = re.compile(r"/^(0?[1-9]|[12][0-9]|3[01])[\/._-](0?[1-9]|1[012])[\/._-]\d{4}$/")  # Your Regex goes between the double quotes
     DOB = input("Enter the date of birth: ")
 
     if re.fullmatch(match, DOB):
